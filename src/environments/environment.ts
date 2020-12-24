c974c239f5d5e3214ea28fe584da9e38f2cd4072
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-24 14:12:36 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCaytBx8FHUu6EIJ4pbzWn-pOWMYxgbnE",
+    authDomain: "eduflor1.firebaseapp.com",
+    projectId: "eduflor1",
+    storageBucket: "eduflor1.appspot.com",
+    messagingSenderId: "777029612326",
+    appId: "1:777029612326:web:801ee7b9d745e00ce8c7e7"
+  }
 };
 
 /*
