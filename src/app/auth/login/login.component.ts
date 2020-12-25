d010aeb364e6b2a9ee1efaee366ba0d564b59d3b
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-24 21:19:47 -0500

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [AuthService],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  Onlogin(){
+    const {email, password} = this.loginForm.value;
+    this.authSvc.login(email,password);
   }
 
 }
