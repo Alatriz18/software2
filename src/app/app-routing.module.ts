837de88f2683e21e4a62c0f8494234ea969ed38d
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-25 00:42:49 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const routes: Routes = [
   {
     path:'verification-email',
     component: SendEmailComponent,
-  }
+  },
+  { path: 'forgot-password', loadChildren: () => import('./auth/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) }
 ];
 
 @NgModule({
