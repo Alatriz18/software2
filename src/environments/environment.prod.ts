244cda1257006cce6e5705964614c3fc92432662
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-27 16:05:43 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCaytBx8FHUu6EIJ4pbzWn-pOWMYxgbnE",
+    authDomain: "eduflor1.firebaseapp.com",
+    projectId: "eduflor1",
+    storageBucket: "eduflor1.appspot.com",
+    messagingSenderId: "777029612326",
+    appId: "1:777029612326:web:801ee7b9d745e00ce8c7e7"
+  }
+}
