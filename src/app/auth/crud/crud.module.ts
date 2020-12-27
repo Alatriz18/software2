a4793def267c56ce3c8dcbc4457c263d5d4c1883
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-27 14:24:31 -0500

    Update crud.module.ts

--- src/app/auth/crud/crud.module.ts
+++ src/app/auth/crud/crud.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CrudRoutingModule } from './crud-routing.module';
 import { CrudComponent } from './crud.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CrudComponent],
   imports: [
     CommonModule,
     CrudRoutingModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    NgbModule,
+    ReactiveFormsModule
   ]
 })
 export class CrudModule { }
