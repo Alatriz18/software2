0b873b5f762b28fad1e41f33d6ad177260127a3e
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-27 16:05:40 -0500

    Create firebase-service.service.ts

---
+++ src/app/services/firebase-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseServiceService {
+
+  constructor( private firestore: AngularFirestore) { }
+
+
+  getEmpleado(){
+    return this.firestore.collection("empleados").snapshotChanges();
+
+  }
+  createEmpleado(empleado:any){
+    return this.firestore.collection("empleados").add(empleado);
+
+  }
+  updateEmpleado(id:any){
+    return this.firestore.collection("empleados").doc(id).update(empleado);
+
+  }
+  deleteEmpleado(id:any){
+    return this.firestore.collection("empleados").doc(id).delete();
+  }
+}
