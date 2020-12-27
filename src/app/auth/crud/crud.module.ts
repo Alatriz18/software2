933fa4c63843c8db99cfe48b86e115c1d735c458
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-27 16:05:35 -0500

    Update crud.module.ts

--- src/app/auth/crud/crud.module.ts
+++ src/app/auth/crud/crud.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CrudRoutingModule } from './crud-routing.module';
 import { CrudComponent } from './crud.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { AngularFireModule} from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [CrudComponent],
   imports: [
     CommonModule,
     CrudRoutingModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ]
 })
 export class CrudModule { }
