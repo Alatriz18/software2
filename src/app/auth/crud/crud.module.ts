02b4dcc415fc195b515d73fbca2008b30e86f240
Alatriz18 <kevin.santana4621@utc.edu.ec>
2020-12-27 14:41:32 -0500

    Revert "Update crud.module.ts"
    This reverts commit bc36a1e3f698fe204078d4b6537afdbcdf539fa8.

--- src/app/auth/crud/crud.module.ts
+++ src/app/auth/crud/crud.module.ts
@@ -1,26 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CrudRoutingModule } from './crud-routing.module';
+import { CrudComponent } from './crud.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [CrudComponent],
   imports: [
-    BrowserModule,
-    NgModule,
-    AppRoutingModule,
-    NgxPaginationModule,
-    ReactiveFormsModule,
-    FormsModule,
-    NgbModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    CommonModule,
+    CrudRoutingModule,
+    NgxPaginationModule
+  ]
 })
 export class CrudModule { }
