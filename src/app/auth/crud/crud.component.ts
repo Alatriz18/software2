ebaa319394226f5452324c2c0207178c8b374e7a
Alatriz18 <kevin.santana4621@utc.edu.ec>
2021-01-06 17:38:35 -0500

    Update crud.component.ts

--- src/app/auth/crud/crud.component.ts
+++ src/app/auth/crud/crud.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalConfig, NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseServiceService } from 'src/app/services/firebase-service.service';
+import { isNullOrUndefined } from 'util';
+import {jsPDF} from 'jspdf';
+
 
 
 @Component({
@@ -11,20 +14,32 @@ import { FirebaseServiceService } from 'src/app/services/firebase-service.servic
 })
 export class CrudComponent implements OnInit {
   closeResult = '';
+  
 
   empleadoForm: FormGroup;
+  idFirabaseActualizar: string;
+  actualizar: boolean;
 
   constructor(config: NgbModalConfig, private modalService: NgbModal, public fb: FormBuilder, private firebaseServiceService: FirebaseServiceService) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
-  }
 
+  }
+//public descargar(){
+ // var doc=new jsPDF();
+  //doc.html(document.getElementById('listEmpleados');
+ // doc.save('Listar empleados.pdf');
+
+//}//
 
   config: any;
   collection = { count: 0, data: [] }
+  
 
   ngOnInit(): void {
+    this.idFirabaseActualizar = "";
+    this.actualizar = false;
 
     this.config = {
       itemsPerpage: 5,
@@ -43,7 +58,7 @@ export class CrudComponent implements OnInit {
       sueldo: ['', Validators.required]
 
     });
-
+    
     this.firebaseServiceService.getEmpleado().subscribe(resp => {
       this.collection.data = resp.map((e: any) => {
         return {
@@ -56,13 +71,17 @@ export class CrudComponent implements OnInit {
           correo: e.payload.doc.data().correo,
           sueldo: e.payload.doc.data().sueldo,
           idFirebase: e.payload.doc.id,
+          
 
         }
+        
       })
+      
     },
       error => {
         console.error(error);
       });
+      
 
   }
 
@@ -80,12 +99,59 @@ export class CrudComponent implements OnInit {
     });
 
   }
+  actualizarEmpleado() {
+    if (!isNullOrUndefined(this.idFirabaseActualizar)) {
+      this.firebaseServiceService.updateEmpleado(this.idFirabaseActualizar, this.empleadoForm.value).then(resp => {
+        this.empleadoForm.reset();
+        this.modalService.dismissAll();
+      }).catch(error => {
+        console.error(error);
+      });
+    }
+  }
+  openEditar(content, item: any) {
+
+    //llenar form para editar
+    this.empleadoForm.setValue({
+      id: item.id,
+      ci: item.ci,
+      nombre: item.nombre,
+      apellido: item.apellido,
+      telefono: item.telefono,
+      afiess: item.afiess,
+      correo: item.correo,
+      sueldo: item.sueldo,
+    });
+    this.idFirabaseActualizar = item.idFirebase;
+    this.actualizar = true;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
   open(content) {
-    this.modalService.open(content);
+    this.actualizar = false;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+ 
+
+
   }
 
 
-
-
-}
